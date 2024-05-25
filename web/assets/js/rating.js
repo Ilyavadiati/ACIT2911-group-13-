46626a81a5fa2de1cadb1a1ba0632c291b664d23
William <112908507+WilliamWangDev@users.noreply.github.com>
2024-05-24 20:21:56 -0700

    Polish rating account and review pages; Add signup button on login page (#22)
    * Configure branch 'front-end' in ci.yml
    
    * Remove Run tests
    
    * Fix 'Hi user' button
    
    * Polish rating, account and reviews pages
    
    * Add signup button on login page

--- web/assets/js/rating.js
+++ web/assets/js/rating.js
@@ -135,3 +135,32 @@ document.addEventListener('DOMContentLoaded', function() {
         return selectElement.options[selectElement.selectedIndex].getAttribute('data-instructor');
     }
 
+    
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const user = getUser();
+        if (user) {
+            document.getElementById('login').style.display = 'none';
+            document.getElementById('signup').style.display = 'none';
+            document.getElementById('user').innerHTML = `Hi, ${user.username}`;
+            document.getElementById('logout').style.display = 'block';
+        } else {
+            document.getElementById('login').style.display = 'block';
+            document.getElementById('signup').style.display = 'block';
+            document.getElementById('user').innerHTML = "";
+            document.getElementById('logout').style.display = 'none';
+        }
+    });
+    
+    document.getElementById('logout').addEventListener('click', function(event) {
+        localStorage.removeItem('user');
+        window.location.href = '/';
+    });
+    
+    // redirect to the account page
+    document.getElementById('user').addEventListener('click', function(event) {
+        const user = getUser();
+        if (user) {
+            window.location.href = `/account/${user.username}`;
+        }
+    });
